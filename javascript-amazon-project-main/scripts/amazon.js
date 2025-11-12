591f4b9fb03e64121364a6a5fef9be238e26ef49
Jeffreysam Johnson <jeffreysamjohnson@Jeffreysams-MacBook-Pro.local>
2025-11-13 00:35:40 +1100

    creating a seperate js script for the products

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,30 +1,5 @@
-//list of products available for sale
-const products=[{
-    image:"images/products/athletic-cotton-socks-6-pairs.jpg",
-    name:"Athletic Cotton Socks - 6 Pairs",
-    rating:{
-        stars:4.5,
-        count:87
-    },
-    pricecents:1090
-},
-{
-    image:"images/products/intermediate-composite-basketball.jpg",
-    name:" Intermediate Size Basketball",
-    rating:{
-        stars:4.0,
-        count:127},
-    pricecents:2095},
-    {
-        image:"images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-        name:"Adults' Plain Cotton T-Shirt - 2 Pack",
-        rating:{
-            stars:4.5,
-            count:56
-        },
-        pricecents:799
-    }
-];
+
+
 
 let productsHTML='';
 //create html for the products and add to the page
@@ -49,7 +24,7 @@ products.forEach((product)=> {
           </div>
 
           <div class="product-price">
-            $${(product.pricecents / 100).toFixed(2)}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
